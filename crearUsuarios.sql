9afd2b16c535711c6230e30cf9248a25e76a94c6
Isamaralo <isa-vdm@hotmail.com>
2017-01-11 15:28:46 +0100

    Update crearUsuarios.sql

--- crearUsuarios.sql
+++ crearUsuarios.sql
@@ -1,3 +1,6 @@
 --------------------------------------------------------
 -- Archivo creado  - viernes-diciembre-09-2016   
 --------------------------------------------------------
+CREATE USER <nombre> IDENTIFIED BY <password>;
+GRANT CONNECT TO <nombre>;
+GRANT ALL PRIVILEGES TO <nombre> IDENTIFIED BY <password>;
